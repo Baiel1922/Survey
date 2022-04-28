2924a12e878794be5f07f4bea48637ff2ecf7784
Baiel1922 <82101579+Baiel1922@users.noreply.github.com>
2022-04-28 12:31:45 +0600

    Update settings.py

--- survey/settings.py
+++ survey/settings.py
@@ -106,7 +106,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'leadev.it@gmail.com'
-EMAIL_HOST_PASSWORD = 'leadevgoodcompany'
+EMAIL_HOST_PASSWORD = 'leadevgoodcompany0521'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
